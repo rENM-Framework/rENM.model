37e57751e45ac894ec671c0fb292e03c61c76d8a
John Schnase <jschnase@mac.com>
2026-05-23 09:27:21 -0600

    Repaired "Hmisc" error

--- R/model-create_ensemble_model.R
+++ R/model-create_ensemble_model.R
@@ -168,7 +168,7 @@ create_ensemble_model <- function(
 
   say("Starting create_ensemble_model for ", alpha_code, " / ", year, " ...")
 
-  invisible(lapply(c("sdm", "raster", "terra", "sp", "Hmisc", "dismo", "maxnet"), must_have_pkg))
+  invisible(lapply(c("sdm", "raster", "terra", "sp", "dismo", "maxnet"), must_have_pkg))
 
   if (!file.exists(occs_fn)) {
     stop(sprintf("Occurrences file not found: %s", occs_fn))
